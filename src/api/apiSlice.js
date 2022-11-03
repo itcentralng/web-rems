aa8f656749ef63e7c0e5be8381175bed85d72610
Nasir Mustapha <teboy.com@gmail.com>
2022-11-03 14:09:26 +0100

    fix token temporary token issue on apislice

--- src/api/apiSlice.js
+++ src/api/apiSlice.js
@@ -6,7 +6,7 @@ const baseQuery = fetchBaseQuery({
 
   prepareHeaders: (headers, { getState }) => {
     const credentials = JSON.parse(localStorage.getItem("credentials"));
-    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njc0OTE3MjYsImlhdCI6MTY2NzQ4MDU2Mywic3ViIjoxLCJyb2xlIjoiYWRtaW4ifQ.7r8_fsMuPkYEAk1WBf3R0wrwHSQf1abrr0W9cqq2Ln8"
+    const token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE2Njc0OTE3MjYsImlhdCI6MTY2NzQ4MDU2Mywic3ViIjoxLCJyb2xlIjoiYWRtaW4ifQ.7r8_fsMuPkYEAk1WBf3R0wrwHSQf1abrr0W9cqq2Ln8"
     headers.set("authorization", `Bearer ${token}`);
     return headers;
   },
